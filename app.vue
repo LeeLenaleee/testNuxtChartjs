15dc99bd3c49b0811626bb4399f6bf905f446e23
Jacco van den Berg <jaccoberg2281@gmail.com>
2022-08-23 23:27:40 +0200

    initial

---
+++ app.vue
@@ -0,0 +1,20 @@
+<template>
+  <canvas id="ff"></canvas>
+</template>
+
+<script setup lang="ts">
+import { Chart, registerables } from "chart.js";
+
+  Chart.register(...registerables);
+  new Chart("ff", {
+    type: "doughnut",
+    data: {
+      labels: ["chart", "js"],
+      datasets: [
+        {
+          data: [2, 5],
+        },
+      ],
+    },
+  });
+</script>
